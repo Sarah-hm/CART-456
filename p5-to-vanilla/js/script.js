3c7bdf1bd2c0da20165e332d6f080a5dba065b29
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2024-01-21 19:56:22 -0500

    finished p5 project, mid-way through "vanilla"

---
+++ p5-to-vanilla/js/script.js
@@ -0,0 +1,6 @@
+function setup() {
+  // Create a canvas tag on the HTML file
+  createCanvas(windowWidth, windowHeight);
+  //   Stylize canvas' CSS to have 210RGB background
+  background(210);
+}
